cb0b2a488792a389446873c8f05ceff04ddc60a9
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-19 12:55:09 -0700

    add routes for dynamic movie IDs

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom'
 
 import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList'
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -15,6 +16,8 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" component={Movie}
+      />
     </div>
   );
 };
