c0ba300b8de5783e485b611519cadec65e4410f9
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-19 13:19:07 -0700

    make each movie in MovieList link to route  containing the individual movie

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,27 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const MovieCard = ({ movie }) => {
-  const { title, director, metascore, stars } = movie;
+const MovieCard = (props) => {
+  const { id, title, director, metascore, stars } = props.movie;
+
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
+      <Link to={`/movies/${id}`}>
+        <div className="movie-card">
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+          <h3>Actors</h3>
+            {(!stars)
+              ? <div>Loading Stars...</div> 
+              : stars.map(star => (
+                <div key={star} className="movie-star">
+                  {star}
+                </div>
+            ))}      
         </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-          {(!stars)
-            ? <div>Loading Stars...</div> 
-            : stars.map(star => (
-              <div key={star} className="movie-star">
-                {star}
-              </div>
-          ))}      
-      </div>
-      <div className="save-button">Save</div>
+        <div className="save-button">Save</div>
+      </Link>
     </div>
   );
 };
